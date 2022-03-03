a2742282e81373429434d9abe4e61c17e0f635c2
shadrqen <shadrqen@outlook.com>
2022-03-03 15:18:23 +0300

    feat: add saving of state to all types of storages

--- src/PersistorOptions.ts
+++ src/PersistorOptions.ts
@@ -1,3 +1,8 @@
-export interface SaveState<State> {
-    (key: string, state: State, storage: Storage): void
+import { Store } from 'vuex'
+
+export interface PersistorOptions<State> {
+    key?: string,
+    storage?: Storage,
+    plugin?: (store: Store<State>) => void
+    saveState?: (key: string, state: State, storage: Storage) => void
 }
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,39 @@
-import { Store, MutationPayload } from 'vuex'
-import { SaveState } from './PersistorOptions'
+import { Store, MutationPayload, Plugin } from 'vuex'
+import { PersistorOptions } from './PersistorOptions'
 
-export const VuexPersistor = <State>(store: Store<State>) : void => {
-  const storage: Storage = window.localStorage
-  const key: string = 'vuex'
+/**
+ * The main vuex persistor class
+ */
+export class VuexPersistor<State> implements PersistorOptions<State> {
+    key: string
+    plugin: Plugin<State>
+    storage: Storage
 
-  // called when the store is initialized
-  store.subscribe((mutation: MutationPayload, state: State) => {
-    // called after every mutation.
-    // The mutation comes in the format of `{ type, payload }`.
-    saveState(key, state, storage)
-  })
+    constructor (options?: PersistorOptions<State>) {
+      this.key = options && options.key ? options.key : 'vuex'
+      this.storage = options && options.storage ? options.storage : window.localStorage
+      this.plugin = (store: Store<State>) : void => {
+        this.subscribe(store)((mutation: MutationPayload, state: State) => {
+          this.saveState(this.key, state, this.storage)
+        })
+      }
+    }
 
-  const saveState: SaveState<State> = (key: string, state: State, storage: Storage) => {
-    storage.setItem(key, JSON.stringify(state))
-  }
+    /**
+     * Saves the state on the storage
+     * @param {string} key - The storage key
+     * @param {State} state - The state to save
+     * @param {Storage} storage - The storage to which to save the state
+     * @returns {void}
+     */
+    saveState (key: string, state: State, storage: Storage): void {
+      storage.setItem(key, JSON.stringify(state))
+    }
+
+    /**
+     * Exposes hooks for each mutation - called after every mutation
+     * @param {object} store - The store instance
+     * @returns {function} - The store.subscribe function that is called after every mutation
+     */
+    private subscribe = (store: Store<State>) => (handler: (mutation: MutationPayload, state: State) => any) => store.subscribe(handler)
 }
