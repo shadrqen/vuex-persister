c240f09796dda6ac7876da9b080d78ef4ed47b8d
shadrqen <shadrqen@outlook.com>
2022-03-03 17:47:40 +0300

    feat: add state rehydration

--- src/PersistorOptions.ts
+++ src/PersistorOptions.ts
@@ -1,8 +1,24 @@
 import { Store } from 'vuex'
 
+export type GetStateUnion<State> = null | State
+
 export interface PersistorOptions<State> {
     key?: string,
+    overwrite?: Boolean,
     storage?: Storage,
-    plugin?: (store: Store<State>) => void
+    persist?: (store: Store<State>) => void
+    getSavedState?: (key: string, storage: Storage) => void
     saveState?: (key: string, state: State, storage: Storage) => void
+}
+
+export interface GetState<State> {
+    (key: string, storage: Storage): GetStateUnion<State>
+}
+
+export interface RehydrateState<State> {
+    (overwrite: Boolean, store: Store<State>, key: string, storage: Storage): void
+}
+
+export interface SaveState<State> {
+    (key: string, state: State, storage: Storage): void
 }
