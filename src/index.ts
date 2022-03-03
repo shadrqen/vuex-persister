eb4bd2c4d0a14c2a918497f5a328da86b086ad0a
shadrqen <shadrqen@outlook.com>
2022-03-03 17:47:40 +0300

    feat: add state rehydration

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
+import merge from 'deepmerge'
 import { Store, MutationPayload, Plugin } from 'vuex'
-import { PersistorOptions } from './PersistorOptions'
+import { PersistorOptions, GetState, GetStateUnion, SaveState, RehydrateState } from './PersistorOptions'
 
 /**
  * The main vuex persistor class
  */
 export class VuexPersistor<State> implements PersistorOptions<State> {
     key: string
-    plugin: Plugin<State>
+    persist: Plugin<State>
     storage: Storage
+    overwrite: Boolean
 
     constructor (options?: PersistorOptions<State>) {
       this.key = options && options.key ? options.key : 'vuex'
+      this.overwrite = options && options.overwrite ? options.overwrite : false
       this.storage = options && options.storage ? options.storage : window.localStorage
-      this.plugin = (store: Store<State>) : void => {
+      this.persist = (store: Store<State>) : void => {
+        this.rehydrateState(this.overwrite, store, this.key, this.storage)
         this.subscribe(store)((mutation: MutationPayload, state: State) => {
           this.saveState(this.key, state, this.storage)
         })
@@ -26,8 +30,55 @@ export class VuexPersistor<State> implements PersistorOptions<State> {
      * @param {Storage} storage - The storage to which to save the state
      * @returns {void}
      */
-    saveState (key: string, state: State, storage: Storage): void {
+    saveState: SaveState<State> = (key: string, state: State, storage: Storage) : void => {
       storage.setItem(key, JSON.stringify(state))
+    }
+
+    /**
+     * Rehydrates the state whenever there is a saved state
+     * @param {boolean} overwrite - Whether to overwrite existing state or not
+     * @param {Store} store - The store instance
+     * @param {string} key - The storage key
+     * @param {Storage} storage - The storage instance
+     * @return {void}
+     */
+    rehydrateState :RehydrateState<State> = (overwrite: Boolean, store: Store<State>, key: string, storage: Storage) : void => {
+      const SAVED_STATE = this.getSavedState(key, storage)
+      if (SAVED_STATE) {
+        store.replaceState(this.overwrite
+          ? SAVED_STATE
+          : merge(store.state, SAVED_STATE, {
+            arrayMerge: (store, saved) => saved
+          })
+        )
+      }
+    }
+
+    /**
+     * Gets the saved state
+     * @param {string} key - The storage key
+     * @param {Storage} storage - The storage to which to save the state
+     * @returns {object|undefined} - The saved state
+     */
+    getSavedState: GetState<State> = (key: string, storage: Storage) : GetStateUnion<State> => {
+      const STATE_VALUE = storage.getItem(key)
+      try {
+        let STATE: GetStateUnion<State>
+        switch (typeof STATE_VALUE) {
+          case 'string':
+            STATE = JSON.parse(STATE_VALUE)
+            break
+          case 'object':
+            STATE = STATE_VALUE
+            break
+          default:
+            STATE = null
+            break
+        }
+        return STATE
+      } catch (err) {
+        return null
+      }
     }
 
     /**
